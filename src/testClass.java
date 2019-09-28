81ce350bf834b2ea503413109b0c961dee48a52c
Dima <mr.tarasiuk@gmail.com>
2019-09-28 03:05:15 +0300

    added test class

---
+++ src/testClass.java
@@ -0,0 +1,20 @@
+public class testClass {
+    private static long id;
+    public static String name;
+
+    public static long getId() {
+        return id;
+    }
+
+    public static void setId(long id) {
+        testClass.id = id;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String name) {
+        testClass.name = name;
+    }
+}
