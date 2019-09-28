19a8f040910d4d65c6156f4c474c0ae5b37da423
Dima <mr.tarasiuk@gmail.com>
2019-09-28 04:09:49 +0300

    added simple logic

--- src/Terminal.java
+++ src/Terminal.java
@@ -4,8 +4,25 @@ import java.util.Scanner;
 public class Terminal {
     public static void main(String[] args) {
         System.out.println("put the card");
-        Scanner cardScan = new Scanner(System.in);
-        boolean active = cardScan.hasNext();
+        Scanner scanner = new Scanner(System.in);
+        String cardScan = scanner.next();
 
+        Card adminCard = new Card(false);
+        adminCard.setPinCode(1111);
+        adminCard.setBalance(232897982);
+
+
+        if (cardScan.equals("y")){
+            while (!adminCard.isBlock){  //TODO active check
+                System.out.println("Enter pin");
+                Scanner pin = new Scanner(System.in);
+
+                if (adminCard.getPinCode()==pin.nextInt()){
+                    System.out.println("your balance is " + adminCard.getBalance() + "$");
+                }else System.out.println("wrong pin");
+            }
+        }else {
+            System.out.println("access denied");
+        }
     }
 }
